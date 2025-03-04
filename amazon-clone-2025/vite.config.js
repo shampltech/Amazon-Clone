9da1bc9d14971ffb3bf1ce0cd4597c2d8a825300
Mohammed <mamegit2020@gmail.com>
2025-03-04 11:45:49 +0300

    final gh-commit

--- amazon-clone-2025/vite.config.js
+++ amazon-clone-2025/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Amazon-Clone/",
+  base: "/amazon-clone-2025/",
   plugins: [react()],
 });
