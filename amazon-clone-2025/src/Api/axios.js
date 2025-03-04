1884fcea05fa7255d482e5cd1a0d6358421d2de3
Mohammed <mamegit2020@gmail.com>
2025-03-04 07:07:47 +0300

    some final stylyng

--- amazon-clone-2025/src/Api/axios.js
+++ amazon-clone-2025/src/Api/axios.js
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-83faf/us-central1/api",
-  baseURL: "https://amazon-api-18m8.onrender.com",
+  baseURL: "https://amazon-api-2-fbj9.onrender.com",
 });
 
 export {axiosInstance}
+
+
+
+
